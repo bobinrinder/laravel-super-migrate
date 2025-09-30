9712d85793ed433cffce2168405c424c2f2946e6
bobinrinder <mail@robinbinder.com>
2025-09-30 15:18:20 -0700

    fixed migration path

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,7 +28,7 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        $migration = include __DIR__.'/../database/migrations/create_super_migrate_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_super_migrate_table.php';
         $migration->up();
     }
 }
